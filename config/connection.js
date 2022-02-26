454329f4d1598ea304d16c8f68fe379bd8362d29
ByrdBassPC <byrdbass@gmail.com>
2022-02-25 19:45:51 -0600

    created readme file

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,22 @@ require('dotenv').config({ path: require('find-config')('.env') });
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL
+    // ,{host: }
+    
+    );
 } 
-// else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// }
+else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
