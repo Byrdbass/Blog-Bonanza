d029ff6c608bd591d3534ba2010ffbb0ca80de9a
ByrdBassPC <byrdbass@gmail.com>
2022-02-25 19:54:27 -0600

    made edits to connection.js for jawsDB url and host

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,13 @@ require('dotenv').config({ path: require('find-config')('.env') });
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL
-    // ,{host: }
-    
-    );
+  sequelize = new Sequelize(process.env.JAWSDB_URL,{
+    host: 'hw5q55w78qsiio80:r15m01xwmrz1yvif@eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tvlcla5ou2ywodjr'
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    } 
+  });
 } 
 else {
   sequelize = new Sequelize(
